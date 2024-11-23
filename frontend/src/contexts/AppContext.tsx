6df7c79b1fbe2ec1ba79e00d9e7a451560eb491d
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-11-23 23:39:37 +0530

    Update AppContext.tsx

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
 import { loadStripe, Stripe } from "@stripe/stripe-js";
 import Toast from "../components/Toast";
+import { UserType } from "../../../backend/src/shared/types";
 const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || "";
 
 type ToastMessage = {
@@ -10,17 +11,13 @@ type ToastMessage = {
   type: "SUCCESS" | "ERROR";
 };
 
-type User = {
-  id: string; 
-  name: string;
- 
-};
+
 
 type AppContext = {
   showToast: (toastMessage: ToastMessage) => void;
   isLoggedIn: boolean;
   stripePromise: Promise<Stripe | null>;
-  currentUser?: User; 
+  currentUser?: UserType; 
 };
 
 const AppContext = React.createContext<AppContext | undefined>(undefined);
@@ -29,7 +26,7 @@ const stripePromise = loadStripe(STRIPE_PUB_KEY);
 
 export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [toast, setToast] = useState<ToastMessage | undefined>(undefined);
-  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
+  const [currentUser, setCurrentUser] = useState<UserType|undefined>(undefined);
 
   const { isError } = useQuery("validateToken", apiClient.validateToken, {
     retry: false,

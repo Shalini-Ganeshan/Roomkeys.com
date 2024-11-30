470b6b813e631514bb9fc154674ae092c88cd969
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-11-30 22:28:55 +0530

    Update Layout.tsx

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -25,7 +25,7 @@ const Layout = ({ children }: Props) => {
 
       </div>
 
-      <div className="container mx-auto  sm:mt-[-20px]  px-4 md:px-8 sm:px-12 max-w-7xl"> 
+       <div className="absolute top-[-30px] left-1/2 transform -translate-x-1/2 px-4 md:px-8 sm:px-12 max-w-7xl z-10">
         <SearchBar />
       </div>
 

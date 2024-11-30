d1f689841f924bdfd570882a61b777cc1dbd743d
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-12-01 02:15:22 +0530

    Update SearchBar.tsx

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -59,7 +59,7 @@ const SearchBar = () => {
       <div className="flex flex-wrap gap-4 items-center justify-between md:flex-row flex-col">
         
         {/* Destination Input */}
-        <div className="flex-2 min-w-[320px] md:min-w-[350px] w-full">
+        <div className="flex-1 min-w-[320px] md:min-w-[350px] w-full">
           <div className="flex items-center border-2 border-indigo-800 shadow-xl rounded-xl bg-white p-1 w-full">
             <MdTravelExplore size={20} className="mr-1" />
             <input

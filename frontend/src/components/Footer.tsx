cbeafa13af620ce7ba1d9d00faaaf3d51455f4b9
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-11-30 23:11:48 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -19,11 +19,11 @@ const Footer = () => {
         <span className="text-lg ml-1 sm:text-sm md:text-xl text-white font-semibold font-playwriteDEGrund tracking-tight">
           RoomKeys.com
         </span>
-        <span className="text-white text-sm sm:text-[3px]  sm:mr-0 md:mr-1 sm:ml-1 md:text-sm tracking-tight flex gap-4">
+        <span className="text-white md:text-sm text-[4px]  mr-0 md:mr-1 sm:ml-1 tracking-tight flex gap-4">
           <Link to="/privacy" className="cursor-pointer">
             Privacy Policy
           </Link>
-          <Link to="/termsofservice" className="cursor-pointer mr-1">
+          <Link to="/termsofservice" className="cursor-pointer md:mr-1">
             Terms of Service
           </Link>
         </span>
